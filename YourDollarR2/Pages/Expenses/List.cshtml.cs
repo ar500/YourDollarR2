df37cfda7eef873ee8080c5b65b42e419491d840
Jason Bradshaw <bradshaw15r@gmail.com>
2019-05-16 21:17:09 -0400

    Added Attach Category functionality

--- YourDollarR2/Pages/Expenses/List.cshtml.cs
+++ YourDollarR2/Pages/Expenses/List.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -10,25 +11,35 @@ namespace YourDollarR2.Pages.Expenses
     public class ListModel : PageModel
     {
         private readonly IExpenseRepository _expenseRepository;
+        private readonly IBudgetCategoryRepository _categoryRepository;
 
         public IEnumerable<ExpenseDto> Expenses { get; set; }
 
         [BindProperty]
+        public IEnumerable<BudgetCategoryDto> Categories { get; set; }
+
+        [BindProperty(SupportsGet = true)]
         public string SearchTerm { get; set; }
 
         [TempData]
         public string Message { get; set; }
 
-        public ListModel(IExpenseRepository expenseRepository)
+        public ListModel(IExpenseRepository expenseRepository, IBudgetCategoryRepository categoryRepository)
         {
             _expenseRepository = expenseRepository;
+            _categoryRepository = categoryRepository;
         }
 
         public void OnGet()
         {
+            // TODO: Make List searches case insensitive.
             var expensesFromRepo = _expenseRepository.GetExpensesByName(SearchTerm);
 
             Expenses = Mapper.Map<IEnumerable<ExpenseDto>>(expensesFromRepo);
+
+            var categoriesFromRepo = _categoryRepository.GetCategoriesByName();
+
+            Categories = Mapper.Map<IEnumerable<BudgetCategoryDto>>(categoriesFromRepo);
         }
     }
 }
